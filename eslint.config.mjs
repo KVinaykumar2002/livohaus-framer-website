af0fac5a194b81c6b063ed7b1693519257d5466a
Kalakonda Vinay kumar <kalakondavinaykumar@Kalakondas-MacBook-Air.local>
2025-11-21 01:52:22 +0530

    Initial commit: Add configuration files

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import nextConfig from 'eslint-config-next'
+import pluginImport from 'eslint-plugin-import'
+
+export default [
+  ...nextConfig,
+  {
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/namespace': 'error',
+      'import/no-absolute-path': 'error',
+      'import/no-dynamic-require': 'error',
+      'import/no-self-import': 'error',
+      'import/no-cycle': 'error',
+      'import/no-useless-path-segments': 'error',
+    },
+  },
+]
